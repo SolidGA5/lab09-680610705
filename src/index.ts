7945b20492e949d730578f4f9208a129a77ad1bb
Mastery <phuphing.gh.chompubang@gmail.com>
2026-07-29 16:58:20 +0700

    version last real

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,7 @@ app.get("/api/me", (req: Request, res: Response) => {
     },
   });
 });
+//
 app.use("/api/v2/users", userRoutes);
 app.use("/api/v2/enrollments", enrollments_v2);
 app.use("/api/v2/students", studentRouter_v2);
